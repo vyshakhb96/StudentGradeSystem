a30baaa08aeca33b5b10438904f2201e35b3de17
Meenu.Prasannakumar <meenu.prasannakumar@ust.com>
2022-03-07 15:56:23 +0530

    final

--- StudentCrudOperation/App_Start/BundleConfig.cs
+++ StudentCrudOperation/App_Start/BundleConfig.cs
@@ -8,17 +8,34 @@ using System.Web.Optimization;
         public static void RegisterBundles(BundleCollection bundle)
         {
             StyleBundle myCssBundle = new StyleBundle("~/Content/MyCSS");
-            myCssBundle.Include("~/Content/bootstrap.min.css", "~/Content/Site.css", "~/Style/StyleSheet.css", "~/Content/DataTables/css/jquery.dataTables.css", "~/Content/toastr.min.css");
-
+            myCssBundle.Include(
+                                 "~/Content/bootstrap.min.css", 
+                                 "~/Content/Site.css",
+                                 "~/Style/StyleSheet.css",
+                                 "~/Content/DataTables/css/jquery.dataTables.css",
+                                 "~/Content/toastr.min.css"
+                                 );
 
             ScriptBundle myScriptBundle = new ScriptBundle("~/Scripts/MyScript");
-            myScriptBundle.Include("~/Scripts/jquery-1.9.1.js", "~/Scripts/jquery-1.9.1.min.js", "~/Scripts/bootstrap.min.js", "~/Scripts/DataTables/jquery.dataTables.js", "~/Scripts/jquery.validate.min.js", "~/Scripts/jquery.validate.unobtrusive.js", "~/Scripts/toastr.min.js", "~/Scripts/jszip.min.js", "~/Scripts/DataTables/dataTables.buttons.min.js", "~/Scripts/DataTables/buttons.html5.min.js","~/Scripts/DataTables/buttons.print.min.js");
+            myScriptBundle.Include(
+                                    "~/Scripts/jquery-1.9.1.js",
+                                    "~/Scripts/jquery-1.9.1.min.js",
+                                    "~/Scripts/bootstrap.min.js",
+                                    "~/Scripts/DataTables/jquery.dataTables.js",
+                                    "~/Scripts/jquery.validate.min.js",
+                                    "~/Scripts/jquery.validate.unobtrusive.js",
+                                    "~/Scripts/toastr.min.js",
+                                    "~/Scripts/jszip.min.js",
+                                    "~/Scripts/DataTables/dataTables.buttons.min.js",
+                                    "~/Scripts/DataTables/buttons.html5.min.js",
+                                    "~/Scripts/DataTables/buttons.print.min.js"
+                                  );
 
 
             bundle.Add(myCssBundle);
             bundle.Add(myScriptBundle);
 
-            BundleTable.EnableOptimizations = true;
+            //BundleTable.EnableOptimizations = true;
 
         }
      }
